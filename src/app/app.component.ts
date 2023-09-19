f185287c4c205acd08bf5a8fe0a2ed312097dd3e
zaq345 <alexdit118@gmail.com>
2023-09-19 19:25:25 +0300

    1.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BookService } from './book.service';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/book/book.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BookService } from '../book.service';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent {
+  constructor(
+    public bookService: BookService,
+  ) { }
+}
