055c1c2038e5ade93b49a44aa936df44f442c8e4
zaq345 <alexdit118@gmail.com>
2023-09-23 19:38:53 +0300

    1.6 v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookComponent } from './book/book.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: '**',
+    redirectTo: 'book',
+  },
+  {
+    path: 'book',
     component: BookComponent,
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
+import { BookService } from './book.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { BookComponent } from './book/book.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
